2a878c719706a9251d0fb372e2189f076654be73
Doğukan Başer <neyazcam2000@gmail.com>
2022-08-11 01:53:20 +0300

    a
    a
    
    Initial commit

---
+++ 194001036_dogukan_baser/Joomla/administrator/logs/upload.error.php
@@ -0,0 +1,104 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-06-01 14:05:45 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-06-01T14:05:45+00:00	INFO ::1	upload	
+2022-06-01T14:22:20+00:00	INFO ::1	upload	
+2022-06-01T14:33:09+00:00	INFO ::1	upload	
+2022-06-01T14:36:22+00:00	INFO ::1	upload	
+2022-06-01T15:23:41+00:00	INFO ::1	upload	
+2022-06-01T15:26:20+00:00	INFO ::1	upload	
+2022-06-01T15:26:20+00:00	INFO ::1	upload	
+2022-06-01T15:26:22+00:00	INFO ::1	upload	File exists: [ROOT]\images\noxus.png by user_id 622
+2022-06-01T15:27:17+00:00	INFO ::1	upload	File exists: [ROOT]\images\dvc.png by user_id 622
+2022-06-01T16:06:58+00:00	INFO ::1	upload	
+2022-06-01T16:06:58+00:00	INFO ::1	upload	
+2022-06-01T16:13:52+00:00	INFO ::1	upload	
+2022-06-01T16:13:52+00:00	INFO ::1	upload	
+2022-06-01T16:16:53+00:00	INFO ::1	upload	File exists: [ROOT]\images\dvc.png by user_id 622
+2022-06-01T16:16:53+00:00	INFO ::1	upload	
+2022-06-01T16:17:06+00:00	INFO ::1	upload	File exists: [ROOT]\images\dvc.png by user_id 622
+2022-06-01T16:24:29+00:00	INFO ::1	upload	
+2022-06-01T16:24:29+00:00	INFO ::1	upload	
+2022-06-01T17:25:59+00:00	INFO ::1	upload	
+2022-06-01T17:25:59+00:00	INFO ::1	upload	
+2022-06-01T17:25:59+00:00	INFO ::1	upload	
+2022-06-01T17:46:00+00:00	INFO ::1	upload	File exists: [ROOT]\images\relia_1.png by user_id 622
+2022-06-01T17:46:00+00:00	INFO ::1	upload	
+2022-06-01T17:46:00+00:00	INFO ::1	upload	
+2022-06-01T17:46:00+00:00	INFO ::1	upload	
+2022-06-01T17:46:00+00:00	INFO ::1	upload	
+2022-06-01T17:46:00+00:00	INFO ::1	upload	
+2022-06-01T17:46:01+00:00	INFO ::1	upload	
+2022-06-01T17:46:01+00:00	INFO ::1	upload	
+2022-06-01T17:46:01+00:00	INFO ::1	upload	
+2022-06-01T17:46:01+00:00	INFO ::1	upload	
+2022-06-01T17:46:01+00:00	INFO ::1	upload	
+2022-06-01T17:51:15+00:00	INFO ::1	upload	
+2022-06-01T17:52:56+00:00	INFO ::1	upload	
+2022-06-01T18:09:56+00:00	INFO ::1	upload	
+2022-06-01T18:09:56+00:00	INFO ::1	upload	
+2022-06-01T18:09:56+00:00	INFO ::1	upload	
+2022-06-01T18:09:56+00:00	INFO ::1	upload	
+2022-06-01T18:09:56+00:00	INFO ::1	upload	
+2022-06-01T18:09:56+00:00	INFO ::1	upload	
+2022-06-01T18:09:56+00:00	INFO ::1	upload	
+2022-06-01T18:09:56+00:00	INFO ::1	upload	
+2022-06-01T18:09:56+00:00	INFO ::1	upload	
+2022-06-01T18:09:57+00:00	INFO ::1	upload	
+2022-06-01T18:12:26+00:00	INFO ::1	upload	
+2022-06-01T18:12:30+00:00	INFO ::1	upload	File exists: [ROOT]\images\Targon.png by user_id 622
+2022-06-01T18:18:14+00:00	INFO ::1	upload	
+2022-06-01T19:34:47+00:00	INFO ::1	upload	
+2022-06-01T19:34:47+00:00	INFO ::1	upload	
+2022-06-01T19:34:47+00:00	INFO ::1	upload	
+2022-06-01T19:34:47+00:00	INFO ::1	upload	
+2022-06-01T19:34:47+00:00	INFO ::1	upload	File exists: [ROOT]\images\Zed.png by user_id 622
+2022-06-01T19:34:47+00:00	INFO ::1	upload	File exists: [ROOT]\images\zed_1.png by user_id 622
+2022-06-01T19:34:47+00:00	INFO ::1	upload	
+2022-06-01T19:34:47+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	File exists: [ROOT]\images\master.png by user_id 622
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	File exists: [ROOT]\images\master_1.png by user_id 622
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:34:48+00:00	INFO ::1	upload	
+2022-06-01T19:35:59+00:00	INFO ::1	upload	
+2022-06-01T19:40:59+00:00	INFO ::1	upload	
+2022-06-01T19:45:05+00:00	INFO ::1	upload	
+2022-06-01T19:49:39+00:00	INFO ::1	upload	
+2022-06-01T19:53:54+00:00	INFO ::1	upload	
+2022-06-01T19:55:33+00:00	INFO ::1	upload	
+2022-06-01T19:57:58+00:00	INFO ::1	upload	File exists: [ROOT]\images\kaisa_1.png by user_id 622
+2022-06-01T19:59:00+00:00	INFO ::1	upload	
+2022-06-01T20:19:13+00:00	INFO ::1	upload	
+2022-06-01T20:33:48+00:00	INFO ::1	upload	
+2022-06-01T20:57:31+00:00	INFO ::1	upload	
+2022-06-01T21:00:54+00:00	INFO ::1	upload	
+2022-06-01T21:04:06+00:00	INFO ::1	upload	
+2022-06-01T21:11:50+00:00	INFO ::1	upload	
+2022-06-01T21:14:19+00:00	INFO ::1	upload	
+2022-06-01T21:19:22+00:00	INFO ::1	upload	
+2022-06-01T21:21:24+00:00	INFO ::1	upload	
+2022-06-01T21:24:37+00:00	INFO ::1	upload	
+2022-06-02T19:34:00+00:00	INFO ::1	upload	
+2022-06-02T20:05:48+00:00	INFO ::1	upload	
+2022-06-02T20:10:34+00:00	INFO ::1	upload	
+2022-06-02T20:11:04+00:00	INFO ::1	upload	
